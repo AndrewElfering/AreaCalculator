57a6556d6040193b697c73406052377654ba2e79
Drew <93015578+AndrewElfering@users.noreply.github.com>
2025-05-14 14:27:21 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+Public class Main {
+	public static void main (string[] args) {
+		// Create integer variables
+		int length = 4;
+		int width = 6;
+		int area;
+
+		// Calculate the width of a rectangle
+		area = length * width;
+
+		// Print Variables
+		System.out.println("Length is: " + length);
+		System.out.println("Width is: " + width);
+		System.out.println("Rectangle area is: " + area);
+	}
+}
