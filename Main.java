d90ff945e4e27f3ae862699544c37105ca552b5f
Andrew Elfering <93015578+AndrewElfering@users.noreply.github.com>
2025-05-22 12:42:50 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,32 @@
 public class Main {
 	public static void main (String[] args) {
-		double area1 = calculateArea(5.5, 4.2);
-		double area2 = calculateArea(7.1, 2.3);
-		double area3 = calculateArea(9.9, 1.2);
+    		double area1 = calculateArea(5.5, 4.2);
+   		double area2 = calculateArea(7.1, 2.3);
+    		double area3 = calculateArea(9.9, 1.2);
+
+		calculateArea(-5.5, 4.2);
+
+		String englishExplanation = explainArea("English");
+		String frenchExplanation = explainArea("French");
+		String spanishExplanation = explainArea("Spanish");
+		String italianExplanation = explainArea("Italian");
 	}
-	
-	public static double calculateArea(double length, double width) {
+    
+   	public static double calculateArea(double length, double width) {
+		if (length < 0 || width < 0){
+			System.out.println("Invalid Dimensions");
+			System.exit(0);
+		}
 		double area = length * width; 
 		return area;
 	}
+     
+    public static String explainArea(String language) {
+        switch(language){
+       	    case "English": return "Area equals length * width";
+            case "French": return "La surface est egale a la longueur * la largeur";
+            case "Spanish": return "Area es igual a largo * ancho";
+            defualt: return "Language not available";
+        }
+    }
 }
