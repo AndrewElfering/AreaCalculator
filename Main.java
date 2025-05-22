3a42f66a9342db1d9ce2f801ae7030dfcecd76c4
Andrew Elfering <93015578+AndrewElfering@users.noreply.github.com>
2025-05-22 11:45:50 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,12 @@
 public class Main {
 	public static void main (String[] args) {
-		calculateArea(5.5, 4.2);
-		calculateArea(7.1, 2.3);
-		calculateArea(9.9, 1.2);
+		double area1 = calculateArea(5.5, 4.2);
+		double area2 = calculateArea(7.1, 2.3);
+		double area3 = calculateArea(9.9, 1.2);
 	}
 	
-	public static void calculateArea(double length, double width) {
+	public static double calculateArea(double length, double width) {
 		double area = length * width; 
-		System.out.println("Area = " + area);
+		return area;
 	}
 }
