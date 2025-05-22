ec31387b75b3359bca46110ca4e03a5f0a4edd66
Andrew Elfering <93015578+AndrewElfering@users.noreply.github.com>
2025-05-22 11:19:03 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,12 @@
-Public class Main {
-	public static void main (string[] args) {
-		// Create integer variables
-		int length = 4;
-		int width = 6;
-		int area;
-
-		// Calculate the area of rectangle
-		area = length * width;
-
-		// Print Variables
-		System.out.println("Length is: " + length);
-		System.out.println("Width is: " + width);
-		System.out.println("Rectangle area is: " + area);
+public class Main {
+	public static void main (String[] args) {
+		calculateArea(5.5, 4.2);
+		calculateArea(7.1, 2.3);
+		calculateArea(9.9, 1.2);
+	}
+	
+	public static void calculateArea(double length, double width) {
+		double area = length * width; 
+		System.out.println("Area = " + area);
 	}
 }
